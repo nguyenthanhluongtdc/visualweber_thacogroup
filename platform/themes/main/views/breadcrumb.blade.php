6ae2c3b5396bf233554ba8c83d1be9b3eb9b267d
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-07-22 10:32:23 +0700

    no message

--- platform/themes/main/views/breadcrumb.blade.php
+++ platform/themes/main/views/breadcrumb.blade.php
@@ -4,7 +4,7 @@
             @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
                 @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
                     <li class="breadcrumb-item">
-                        <a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a>
+                        <a href="{{ $crumb['url'] }}">Trang chủ</a>
                     </li>
                 @else
                     <li class="breadcrumb-item active">{!! $crumb['label'] !!}</li>
