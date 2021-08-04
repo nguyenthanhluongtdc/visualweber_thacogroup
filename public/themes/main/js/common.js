5c44d47bd9c3f9f05bb45305542b4a7c1a4b003c
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-08-04 15:04:56 +0700

    no message

--- public/themes/main/js/common.js
+++ public/themes/main/js/common.js
@@ -321,6 +321,33 @@ if ($('.field-activity-slide-top').length > 0) {
     })
 }
 
+//scroll play video
+window.addEventListener('load', videoScroll);
+window.addEventListener('scroll', videoScroll);
+
+function videoScroll() {
+
+    if (document.querySelectorAll('video[autoplay]').length > 0) {
+        var windowHeight = window.innerHeight,
+            videoEl = document.querySelectorAll('video[autoplay]');
+
+        for (var i = 0; i < videoEl.length; i++) {
+
+            var thisVideoEl = videoEl[i],
+                videoHeight = thisVideoEl.clientHeight,
+                videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+            if (videoClientRect <= ((windowHeight) - (videoHeight * .5)) && videoClientRect >= (0 - (videoHeight * .5))) {
+                thisVideoEl.play();
+            } else {
+                thisVideoEl.pause();
+            }
+
+        }
+    }
+
+}
+
 // change color header
 if ($('#header').length > 0) {
     var header = $('#header')
