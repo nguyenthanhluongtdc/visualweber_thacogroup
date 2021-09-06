4032505682ef85414f1a1d1af566f614b6ab4593
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-09-06 16:43:22 +0700

    sass

--- platform/themes/main/views/index.blade.php
+++ platform/themes/main/views/index.blade.php
@@ -8,7 +8,7 @@
             <div class="bg-post">
                @if ($post = get_featured_posts(1,[]))
               
-                <div class="content {{has_field($page, 'show_hide') == 'show' ? 'd-block' : 'd-none'}} " data-aos="zoom-in-up" data-aos-duration="1000" data-aos-delay="50" class="aos-init aos-animate">
+                <div class="content {{has_field($page, 'show_hide') == 'hide' ? 'd-none' : ''}} " data-aos="zoom-in-up" data-aos-duration="1000" data-aos-delay="50" class="aos-init aos-animate">
                     <h2 class="font24 title-post">
                     {{$post[0]->name}}
                     </h2>
@@ -37,50 +37,50 @@
         {!! do_shortcode('[field-activity][/field-activity]') !!}
     </div>
 </div>
-<div class="partner-wrapper">
-    <h3 class="title__company font40" data-aos="fade-right" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate"> {{has_field($page, 'tieu_de') ? get_field($page, 'tieu_de') : ''}}</h3>
-    <div class="partner-banner parallax-window" data-parallax="scroll">
-        <div class="container-customize logo-partner"> 
-            <div class="swiper-container logo-company" style="display:none">
-                <div class="swiper-wrapper">   
-                    <div class="swiper-slide" >
-                        <img src="{{Theme::asset()->url('images/home/thilogi-logo.png')}}" alt="">
+@if(has_field($page , 'show_hide_company'))
+    <div class="partner-wrapper {{get_field($page, 'show_hide_company') == 'hide_company' ? 'd-none':''}}"  style="background-image:url({{ has_field($page , 'image_bg_company') ? get_object_image(get_field($page , 'image_bg_company')) :''}})">
+        <h3 class="title__company font40" data-aos="fade-right" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate"> {{has_field($page, 'tieu_de') ? get_field($page, 'tieu_de') : ''}}</h3>
+        <div class="partner-banner parallax-window" data-parallax="scroll">
+            <div class="container-customize logo-partner"> 
+                <div class="swiper-container logo-company" style="display:none">
+                    <div class="swiper-wrapper">   
+                        <div class="swiper-slide" >
+                            <img src="{{Theme::asset()->url('images/home/thilogi-logo.png')}}" alt="">
+                        </div>
+                        <div class="swiper-slide" >
+                            <img src="{{Theme::asset()->url('images/home/thadico-logo.png')}}" alt="">
+                        </div>
+                        <div class="swiper-slide" >
+                            <img src="{{Theme::asset()->url('images/home/thacoauto-logo.png')}}" alt="">
+                        </div>
+                        <div class="swiper-slide" >
+                            <img src="{{Theme::asset()->url('images/home/thagrico-logo.png')}}" alt="">
+                        </div>
+                        <div class="swiper-slide" >
+                            <img src="{{Theme::asset()->url('images/home/thiso-logo.png')}}" alt="">
+                        </div>
                     </div>
-                    <div class="swiper-slide" >
-                        <img src="{{Theme::asset()->url('images/home/thadico-logo.png')}}" alt="">
-                    </div>
-                    <div class="swiper-slide" >
-                        <img src="{{Theme::asset()->url('images/home/thacoauto-logo.png')}}" alt="">
-                    </div>
-                    <div class="swiper-slide" >
-                        <img src="{{Theme::asset()->url('images/home/thagrico-logo.png')}}" alt="">
-                    </div>
-                    <div class="swiper-slide" >
-                        <img src="{{Theme::asset()->url('images/home/thiso-logo.png')}}" alt="">
-                    </div>
+                    <div class="swiper-button-next"><img src="{{Theme::asset()->url("images/home/right-arrow.png")}}" alt="{{_('Next icon')}}"></div>
+                    <div class="swiper-button-prev"><img src="{{Theme::asset()->url("images/home/left-arrow.png")}}" alt="{{_('Prevous icon')}}"></div>
                 </div>
-                <div class="swiper-button-next"><img src="{{Theme::asset()->url("images/home/right-arrow.png")}}" alt="{{_('Next icon')}}"></div>
-                <div class="swiper-button-prev"><img src="{{Theme::asset()->url("images/home/left-arrow.png")}}" alt="{{_('Prevous icon')}}"></div>
-            </div>
-            <div class="logo-desktop">
-              
-                @if(has_field($page,'logo_company'))
-                @foreach (has_field($page,'logo_company') as $logo_item)
-                    
-              
-                <div class="logo-item"  data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                    <img class=""  src="{{ has_sub_field($logo_item , 'image') ? get_object_image(get_sub_field($logo_item , 'image')) :''}}" alt="">
+                <div class="logo-desktop">
+                
+                    @if(has_field($page,'logo_company'))
+                    @foreach (has_field($page,'logo_company') as $logo_item)
+                        
+                
+                    <div class="logo-item"  data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
+                        <img class=""  src="{{ has_sub_field($logo_item , 'image') ? get_object_image(get_sub_field($logo_item , 'image')) :''}}" alt="">
+                    </div>
+                    @endforeach
+                    @endif
                 </div>
-                @endforeach
-                @endif
             </div>
         </div>
     </div>
-</div>
+@endif
 {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
-{{-- {!! do_shortcode('[news-home][/news-home]') !!}
-{!! do_shortcode('[media-home][/media-home]') !!}
-{!! do_shortcode('[recruitment][/recruitment]') !!} --}}
+
 
 
  
