1fb8d19e22d43edf1711e10e710e95665e4c75eb
Trúc Huỳnh <dntrh0425@gmail.com>
2021-09-08 17:21:57 +0700

    sass

--- platform/themes/main/views/pages/post/slide.blade.php
+++ platform/themes/main/views/pages/post/slide.blade.php
@@ -1,16 +1,21 @@
 <section class="slide-info">
     <div class="swiper-container main-slider" style="--swiper-navigation-color:#fff; --swiper-pagination-color:#fff;">
         <div class="swiper-wrapper">   
+
+            
             <div class="swiper-slide" >
             <img src="{{ Theme::asset()->url('images/relationship/quan-he-co dong-banner.jpg') }}" alt="" class="img-slider  h-45vw w-100">
-        </div>
-            <div class="swiper-slide" >
+            </div>
+
+
+
+            {{-- <div class="swiper-slide" >
                 <img src="{{ Theme::asset()->url('images/media/banner-1.jpg') }}" alt="" class="img-slider  h-45vw  w-100 ">
             </div>
             
             <div class="swiper-slide" >
                 <img src="{{ Theme::asset()->url('images/home/banner-3.jpg') }}" alt="" class="img-slider  h-45vw  w-100 ">
-            </div> 
+            </div>  --}}
         </div>
        
 
