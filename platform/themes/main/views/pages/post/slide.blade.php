f870caaa7647d01b8fe88283ff83806c93899553
Trúc Huỳnh <dntrh0425@gmail.com>
2021-09-09 09:59:18 +0700

    sass

--- platform/themes/main/views/pages/post/slide.blade.php
+++ platform/themes/main/views/pages/post/slide.blade.php
@@ -2,11 +2,13 @@
     <div class="swiper-container main-slider" style="--swiper-navigation-color:#fff; --swiper-pagination-color:#fff;">
         <div class="swiper-wrapper">   
 
-            
+            @if(has_field($category, 'repeater_slider'))
+            @foreach(has_field($category, 'repeater_slider') as $item)
             <div class="swiper-slide" >
-            <img src="{{ Theme::asset()->url('images/relationship/quan-he-co dong-banner.jpg') }}" alt="" class="img-slider  h-45vw w-100">
+            <img src="{{ Storage::disk('public')->exists(has_sub_field($item,'image')) ? get_image_url(has_sub_field($item,'image')) : RvMedia::getDefaultImage()}}" alt="" class="img-slider  h-45vw w-100">
             </div>
-
+            @endforeach
+            @endif
 
 
             {{-- <div class="swiper-slide" >
