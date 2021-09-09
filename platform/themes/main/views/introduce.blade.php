b7ea721e8f6436d8d343527b06d987d38badd0b6
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-09-09 17:22:41 +0700

    sass

--- platform/themes/main/views/introduce.blade.php
+++ platform/themes/main/views/introduce.blade.php
@@ -47,86 +47,101 @@
                 <div class="leader-of-us-wrapper mt-40">
                     <div class="leader-of-us__title mt-40" data-aos="fade-right" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
                         <img src="{{ Theme::asset()->url('images/introduce/arrow.png') }}" alt="">
-                        <h2 class="font50 big-title">Ban lãnh đạo</h2>
+                        <h2 class="font50 big-title">   {!!has_field($page, 'leadership_title')!!}</h2>
                     </div>
-                    <div class="title-admin-top">
-                        <h3 class="title-admin">HỘI ĐỒNG QUẢN TRỊ THACO</h3>
+                  
+                    @foreach (has_field($page, 'council') as $key => $item_council)
+                   
+                    <div class="title-admin-top ">
+                        <h3 class="title-admin">{{has_sub_field($item_council, 'council_name')}}</h3>
                     </div>
-                    <div class="admin-top-content" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                        <div class="row-1">
-                            <div class="admin-top-item " data-target="#info_admin_modal" data-toggle="modal">
-                                <img src="{{ Theme::asset()->url('images/introduce/tbd.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Trần Bá Dương</strong></p>  
-                                <p class="postion">Chủ tịch HĐQT</p>
+                    <div class="admin-content {{$key == 0 ? 'top' : 'bottom'}}" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
+                        @foreach (has_sub_field($item_council, 'council_member') as $item_member)
+                        <div class="item-member">
+                            <div class="admin-item" data-target="#info_admin_modal" data-toggle="modal">
+                                <img src="{{ get_image_url(has_sub_field($item_member, 'image')) }}" alt=" {!! has_sub_field($item_member, 'name')!!}">
+                               
+                                <div class="name">
+                                    {!! has_sub_field($item_member, 'name')!!}
+                                </div>
+                                <div class="postion">
+                                    {!! has_sub_field($item_member, 'position')!!}
+                                </div>
                             </div>
                         </div>
-                        
-                        <div class="row-2">
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/nhm.jpg') }}" alt="">
-                                <p class="name">Ông <strong> Nguyễn Hùng Minh</strong></p>
-                                <p class="postion">Phó Chủ tịch TT HĐQT</p>
-                            </div>
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/kim-teck.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Cheah Kim Teck</strong></p>
-                                <p class="postion">Phó Chủ tịch HĐQT</p>
-                            </div>
-                            
-                        </div>
-                        <div class="row-3">
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/vdh.jpg') }}" alt="">
-                                <p class="name">Bà <strong> Viên Diệu Hoa</strong></p>
-                                <p class="postion">Thành viên</p>
-                            </div>
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/pvt.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Phạm Văn Tài</strong></p>
-                                <p class="postion">Thành viên</p>
-                            </div>
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/setephen.jpg') }}" alt="">
-                                <p class="name">Ông <strong> Stephen Patrick Gore</strong></p>
-                                <p class="postion">Thành viên</p>
-                            </div>
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/nqb.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Nguyễn Quang Bảo</strong></p>
-                                <p class="postion">Thành viên</p>
-                            </div>
-                            <div class="admin-top-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/lts.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Lê Trọng Sánh</strong></p>
-                                <p class="postion">Thành viên</p>
-                            </div>
-                        </div>
-                    
-                        
+                       
+                        @endforeach
                     </div>
-                    <div class="title-admin-bottom" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                        <h3 class="title-admin">Ban kiểm soát</h3>
-                    </div>
-                    <div class="admin-bottom-content">
-                        <div class="bottom-content">
-                            <div class="admin-bottom-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/npt.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Nguyễn Phúc Thịnh</strong></p>  
-                                <p class="postion">Trưởng ban</p>
-                            </div>
-                            <div class="admin-bottom-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/dct.jpg') }}" alt="">
-                                <p class="name">Ông <strong> Đặng Công Trực</strong></p>  
-                                <p class="postion">Thành viên</p>
-                            </div>
-                            <div class="admin-bottom-item" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
-                                <img src="{{ Theme::asset()->url('images/introduce/bmk.jpg') }}" alt="">
-                                <p class="name">Ông <strong>Bùi Minh Khoa</strong></p>  
-                                <p class="postion">Thành viên</p>
+                    @endforeach
+                    <div class="modal fade" id="info_admin_modal" tabindex="-1" role="dialog" aria-labelledby="info_admin_modallLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body mCustomScrollbar p-0" data-mcs-theme="dark">
+                                    
+                                    <div class="row mr-md-0">
+                                       
+                                        <div class="col-md-4 p-0 col-12 col-right pl-md-4">
+                                            <img class="w-100" src="{{Theme::asset()->url('images/introduce/anh-modal.jpg')}}" alt="">
+                                           
+                                        </div>
+                                        <div class="col-md-8">
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <i class="fal fa-times"></i>
+                                                </button>
+                                            
+                                            <div class="info-admin">
+                                                <p class="name">Tên  : <strong class="">Trần Bá Dương</strong>  </p>
+                                                <p class="bof">Sinh năm  : <span>01/04/1960</span> </p>
+                                                <p class="addres-bof">Nơi sinh  : <span>TP. Huế, tỉnh Thừa Thiên Huế                            </span> </p>
+                                            </div>
+                                                
+                                        </div>
+                                    </div>
+                                   
+                                    <div class="work-progress">
+                                        <h3 class="title font20">QUÁ TRÌNH LÀM VIỆC TẠI THACO</h3>
+                                        <div class="work-progress-table">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                    
+                                                    <th scope="col">Vị trí</th>
+                                                    <th scope="col">Tổ chức</th>
+                                                    <th scope="col">Thời gian bổ nhiệm</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                    <td>Lorem Ipsum</td>
+                                                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
+                                                    <td>8/1/2021</td> 
+                                                    </tr>
+                                                    <tr>
+                                                    <td>Lorem Ipsum</td>
+                                                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
+                                                    <td>8/1/2021</td> 
+                                                    </tr>
+                                                    <tr>
+                                                    <td>Lorem Ipsum</td>
+                                                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
+                                                    <td>8/1/2021</td> 
+                                                    </tr>
+                                                    <tr>
+                                                    <td>Lorem Ipsum</td>
+                                                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
+                                                    <td>8/1/2021</td> 
+                                                    </tr>
+                                                    
+                                                </tbody>
+                                                </table>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+               
                 <div class="achievement-wrapper mt-40">
                     <div class="achievement-tab-title">
                         <div class="achievement__title mt-40" data-aos="fade-right" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
@@ -142,8 +157,6 @@
                                     </a>
                                 </li>
                                 @endforeach
-                    
-                                
                             </ul>
                         
                         </div>
@@ -160,7 +173,7 @@
                                         <div class="bottom-content" data-aos="fade-up" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate" >
                                             <div class="img-content">
                                                 {{-- <img src="{{ get_sub_field($item_achivement_year, 'image') ? get_image_url(get_sub_field($item_achivement_year, 'image')) :'' }}" alt="{!!has_sub_field($item_achivement_year, 'achivement_name')!!}"> --}}
-                                                <img src="{{ get_image_url(has_sub_field($item_achivement_year, 'image')) }}" alt="">
+                                                <img src="{{ get_image_url(has_sub_field($item_achivement_year, 'image')) }}" alt="{{has_sub_field($item_achivement_year, 'year')}}">
                                                 <div class="year font24">
                                                     {{has_sub_field($item_achivement_year, 'year')}}
                                                 </div>
@@ -177,7 +190,6 @@
                     </div>
                 </div>    
     </div>
-    
     <div class="develop-wrapper">
         <div class="develop__title pb-40 container-customize" data-aos="fade-right" data-aos-duration="700" data-aos-delay="50" class="aos-init aos-animate">
             <img class="img-blue" src="{{ Theme::asset()->url('images/introduce/arrow.png') }}"  alt="icon">
@@ -242,71 +254,3 @@
     
 </section>
 
-<div class="modal fade" id="info_admin_modal" tabindex="-1" role="dialog" aria-labelledby="info_admin_modallLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-body mCustomScrollbar p-0" data-mcs-theme="dark">
-                
-                <div class="row mr-md-0">
-                   
-                    <div class="col-md-4 p-0 col-12 col-right pl-md-4">
-                        <img class="w-100" src="{{Theme::asset()->url('images/introduce/anh-modal.jpg')}}" alt="">
-                       
-                    </div>
-                    <div class="col-md-8">
-                        
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <i class="fal fa-times"></i>
-                            </button>
-                        
-                        <div class="info-admin">
-                            <p class="name">Tên  : <strong class="">Trần Bá Dương</strong>  </p>
-                            <p class="bof">Sinh năm  : <span>01/04/1960</span> </p>
-                            <p class="addres-bof">Nơi sinh  : <span>TP. Huế, tỉnh Thừa Thiên Huế                            </span> </p>
-                        </div>
-                            
-                    </div>
-                </div>
-               
-                <div class="work-progress">
-                    <h3 class="title font20">QUÁ TRÌNH LÀM VIỆC TẠI THACO</h3>
-                    <div class="work-progress-table">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                
-                                <th scope="col">Vị trí</th>
-                                <th scope="col">Tổ chức</th>
-                                <th scope="col">Thời gian bổ nhiệm</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td>Lorem Ipsum</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
-                                <td>8/1/2021</td> 
-                                </tr>
-                                <tr>
-                                <td>Lorem Ipsum</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
-                                <td>8/1/2021</td> 
-                                </tr>
-                                <tr>
-                                <td>Lorem Ipsum</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
-                                <td>8/1/2021</td> 
-                                </tr>
-                                <tr>
-                                <td>Lorem Ipsum</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing	</td>
-                                <td>8/1/2021</td> 
-                                </tr>
-                                
-                            </tbody>
-                            </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
