c2aaf7e70d22c3122141bd4438a0b155ab08c7a7
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-09-11 18:59:15 +0700

    sass

--- platform/themes/main/views/media.blade.php
+++ platform/themes/main/views/media.blade.php
@@ -243,56 +243,6 @@
                     @if(!empty($albumImage))
                         {{ $albumImage->links('vendor.pagination.custom') }}
                     @endif
-                    {{-- <div class="page-pagination mt-40 mb-40">
-                        <ul class="pagination font18">
-                            <li class="page-item active">
-                                <a href="" class="page-link">
-                                    1
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    2
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    3
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    4
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    5
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    6
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    7
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    >
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="" class="page-link">
-                                    >>
-                                </a>
-                            </li>
-                           
-                        </ul>
-                     </div> --}}
                 </div>
             </div>
 
