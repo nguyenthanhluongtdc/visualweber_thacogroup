67e49e238f47ee9bf6079ea8572fa93366b85510
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-09-22 14:16:27 +0700

    fix download

--- public/themes/main/js/app.js
+++ public/themes/main/js/app.js
@@ -493,7 +493,7 @@ Axios.prototype.request = function request(config) {
     var chain = [dispatchRequest, undefined];
 
     Array.prototype.unshift.apply(chain, requestInterceptorChain);
-    chain = chain.concat(responseInterceptorChain);
+    chain.concat(responseInterceptorChain);
 
     promise = Promise.resolve(config);
     while (chain.length) {
@@ -1036,21 +1036,6 @@ function getDefaultAdapter() {
   return adapter;
 }
 
-function stringifySafely(rawValue, parser, encoder) {
-  if (utils.isString(rawValue)) {
-    try {
-      (parser || JSON.parse)(rawValue);
-      return utils.trim(rawValue);
-    } catch (e) {
-      if (e.name !== 'SyntaxError') {
-        throw e;
-      }
-    }
-  }
-
-  return (encoder || JSON.stringify)(rawValue);
-}
-
 var defaults = {
 
   transitional: {
@@ -1083,7 +1068,7 @@ var defaults = {
     }
     if (utils.isObject(data) || (headers && headers['Content-Type'] === 'application/json')) {
       setContentTypeIfUnset(headers, 'application/json');
-      return stringifySafely(data);
+      return JSON.stringify(data);
     }
     return data;
   }],
@@ -2877,7 +2862,7 @@ swiper_js_swiper_esm__WEBPACK_IMPORTED_MODULE_4__.Swiper.use([swiper_js_swiper_e
                 return this.$http.post('api/download/album/image/', {
                   id: id
                 }).then(function (response) {
-                  console.log(response.message);
+                  window.location = response.data;
                 })["catch"](function (error) {
                   console.log(error);
                 });
@@ -4544,7 +4529,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_laravel_mix_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.pagination {\r\n    width: 100%;\r\n    justify-content: center;\r\n    margin: 40px 0;\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.pagination {\n    width: 100%;\n    justify-content: center;\n    margin: 40px 0;\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -36565,67 +36550,7 @@ var render = function() {
                           )
                         ]),
                         _vm._v(" "),
-                        _c(
-                          "div",
-                          { staticClass: "filter", attrs: { id: "filter" } },
-                          [
-                            _vm._m(4),
-                            _vm._v(" "),
-                            _c("div", { staticClass: "filler-list" }, [
-                              _c(
-                                "div",
-                                { staticClass: "col-md-12 col-12 search-cate" },
-                                _vm._l(_vm.menuFilterImage, function(item, i) {
-                                  return _c(
-                                    "div",
-                                    {
-                                      staticClass: "pretty p-default p-smooth"
-                                    },
-                                    [
-                                      _c("input", {
-                                        directives: [
-                                          {
-                                            name: "model",
-                                            rawName: "v-model",
-                                            value: _vm.changeFilterPhoto,
-                                            expression: "changeFilterPhoto"
-                                          }
-                                        ],
-                                        attrs: { type: "radio" },
-                                        domProps: {
-                                          value: item.reference_id,
-                                          checked: _vm._q(
-                                            _vm.changeFilterPhoto,
-                                            item.reference_id
-                                          )
-                                        },
-                                        on: {
-                                          change: function($event) {
-                                            _vm.changeFilterPhoto =
-                                              item.reference_id
-                                          }
-                                        }
-                                      }),
-                                      _vm._v(" "),
-                                      _c(
-                                        "div",
-                                        { staticClass: "state p-primary" },
-                                        [
-                                          _c("label", [
-                                            _vm._v(
-                                              " " + _vm._s(item.title) + " "
-                                            )
-                                          ])
-                                        ]
-                                      )
-                                    ]
-                                  )
-                                }),
-                                0
-                              )
-                            ])
-                          ]
-                        ),
+                        _vm._m(3),
                         _vm._v(" "),
                         _vm._m(4)
                       ])
@@ -37624,10 +37549,64 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "filter__title" }, [
-      _c("label", { attrs: { for: "" } }, [_vm._v("Ngành nghề ")]),
+    return _c("div", { staticClass: "filter", attrs: { id: "filter" } }, [
+      _c("div", { staticClass: "filter__title" }, [
+        _c("label", { attrs: { for: "" } }, [_vm._v("Filter")]),
+        _vm._v(" "),
+        _c("i", { staticClass: "fas fa-angle-down" })
+      ]),
       _vm._v(" "),
-      _c("i", { staticClass: "fas fa-angle-down" })
+      _c("div", { staticClass: "filler-list" }, [
+        _c("div", { staticClass: "col-md-12 col-12 search-cate" }, [
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("THACO")])
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("Ô tô & Cơ khí")])
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("Nông Lâm Nghiệp")])
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("Đầu tư - Xây Dựng")])
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("Thương mại - Dịch vụ")])
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "pretty p-default p-smooth" }, [
+            _c("input", { attrs: { type: "checkbox" } }),
+            _vm._v(" "),
+            _c("div", { staticClass: "state p-primary" }, [
+              _c("label", [_vm._v("Logistics")])
+            ])
+          ])
+        ])
+      ])
     ])
   },
   function() {
@@ -37775,7 +37754,7 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "filter", attrs: { id: "filter" } }, [
       _c("div", { staticClass: "filter__title" }, [
-        _c("label", { attrs: { for: "" } }, [_vm._v("Filter ")]),
+        _c("label", { attrs: { for: "" } }, [_vm._v("Ngành nghề ")]),
         _vm._v(" "),
         _c("i", { staticClass: "fas fa-angle-down" })
       ]),
@@ -50524,7 +50503,7 @@ Vue.compile = compileToFunctions;
 /***/ ((module) => {
 
 "use strict";
-module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"Promise based HTTP client for the browser and node.js","main":"index.js","scripts":{"test":"grunt test","start":"node ./sandbox/server.js","build":"NODE_ENV=production grunt build","preversion":"npm test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json","postversion":"git push && git push --tags","examples":"node ./examples/server.js","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","fix":"eslint --fix lib/**/*.js"},"repository":{"type":"git","url":"https://github.com/axios/axios.git"},"keywords":["xhr","http","ajax","promise","node"],"author":"Matt Zabriskie","license":"MIT","bugs":{"url":"https://github.com/axios/axios/issues"},"homepage":"https://axios-http.com","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"jsdelivr":"dist/axios.min.js","unpkg":"dist/axios.min.js","typings":"./index.d.ts","dependencies":{"follow-redirects":"^1.14.0"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}]}');
+module.exports = JSON.parse('{"_args":[["axios@0.21.2","/home/thanh-luong/Documents/thacogroup_try_vuejs"]],"_development":true,"_from":"axios@0.21.2","_id":"axios@0.21.2","_inBundle":false,"_integrity":"sha512-87otirqUw3e8CzHTMO+/9kh/FSgXt/eVDvipijwDtEuwbkySWZ9SBm6VEubmJ/kLKEoLQV/POhxXFb66bfekfg==","_location":"/axios","_phantomChildren":{},"_requested":{"type":"version","registry":true,"raw":"axios@0.21.2","name":"axios","escapedName":"axios","rawSpec":"0.21.2","saveSpec":null,"fetchSpec":"0.21.2"},"_requiredBy":["#DEV:/"],"_resolved":"https://registry.npmjs.org/axios/-/axios-0.21.2.tgz","_spec":"0.21.2","_where":"/home/thanh-luong/Documents/thacogroup_try_vuejs","author":{"name":"Matt Zabriskie"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"bugs":{"url":"https://github.com/axios/axios/issues"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}],"dependencies":{"follow-redirects":"^1.14.0"},"description":"Promise based HTTP client for the browser and node.js","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"homepage":"https://axios-http.com","jsdelivr":"dist/axios.min.js","keywords":["xhr","http","ajax","promise","node"],"license":"MIT","main":"index.js","name":"axios","repository":{"type":"git","url":"git+https://github.com/axios/axios.git"},"scripts":{"build":"NODE_ENV=production grunt build","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","examples":"node ./examples/server.js","fix":"eslint --fix lib/**/*.js","postversion":"git push && git push --tags","preversion":"npm test","start":"node ./sandbox/server.js","test":"grunt test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json"},"typings":"./index.d.ts","unpkg":"dist/axios.min.js","version":"0.21.2"}');
 
 /***/ })
 
