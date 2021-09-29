67eb3f018cc79a6c48494c2bc7bb5f54fedc83de
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-09-29 10:01:58 +0700

    fix

--- platform/themes/main/partials/templates/media-news.blade.php
+++ platform/themes/main/partials/templates/media-news.blade.php
@@ -29,21 +29,22 @@
         @forelse($posts as $item)
         <div class="report-item">
             <div class="thumb-img report-item-left">
-                @if(has_field($item, 'repeater_file_media'))
-                <a href="{{get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file'))}}" target="_blank"><p class="name-file font18">
+            
+                <a href="{{ has_field($item, 'repeater_file_media') ? get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file')) : '#'}}" target="_blank"><p class="name-file font18">
                     <img src="{{ Storage::disk('public')->exists($item->image) ? get_object_image($item->image): RvMedia::getDefaultImage() }}"
                     alt="report">
                 </a>
-                @endif
+               
             </div>
             <div class="report-item-right">
                 <span class="date">{{$item->created_at->format('d-m-Y')}}</span>
-                <a href="{{get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file'))}}" target="_blank"><p class="name-file font18"> {!! $item->name !!} </p></a>
+                <a href="{{ has_field($item, 'repeater_file_media') ? get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file')) : '#'}}" target="_blank"><p class="name-file font18"> {!! $item->name !!} </p></a>
                 <span class="date-mobile">{{$item->created_at->format('d-m-Y')}}</span>
                 <div class="download">
-                    <a download href="{{get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file'))}}"
+                    <a download href="{{ has_field($item, 'repeater_file_media') ? get_object_image(has_sub_field(has_field($item, 'repeater_file_media')[0], 'file')) : '#'}}"
                         title="download">{!! __('DOWNLOAD') !!}</a>
-                </div>  
+                </div> 
+                
             </div>
             
         </div>
