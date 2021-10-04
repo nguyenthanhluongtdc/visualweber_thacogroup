0267aae9f0afb330602e41c8d72f4cbb6d565b9b
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-10-04 12:24:12 +0700

    fix

--- platform/themes/main/partials/footer.blade.php
+++ platform/themes/main/partials/footer.blade.php
@@ -126,8 +126,30 @@
             AOS.init({
                 easing: 'ease-in-out-sine'
             });
-            // $('.datepicker').datepicker();
     
+        </script>
+
+        <script>
+            window.fbAsyncInit = function() {
+                    FB.init({
+                        xfbml            : true,
+                        version          : 'v7.0'
+                    });
+                };
+
+                (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+
+                let buttonMes = $('.messenger');
+                buttonMes.on('click', function(e) {
+                    FB.CustomerChat.show();
+                });
+
         </script>
         
         
