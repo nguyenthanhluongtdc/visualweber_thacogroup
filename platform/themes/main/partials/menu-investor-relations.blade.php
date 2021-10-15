c3d310fc4f01f61cbb374da82418aaa88d90fca1
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-10-15 10:53:23 +0700

    sass

--- platform/themes/main/partials/menu-investor-relations.blade.php
+++ platform/themes/main/partials/menu-investor-relations.blade.php
@@ -17,19 +17,35 @@
                 @endforeach
         </div>
     </div>
+    <div class="list-post-new">
+        <div class="wrap">
+            <h2 class="font-mi-bold font30" data-aos="fade-left" data-aos-duration="400" data-aos-delay="50" class="aos-init aos-animate">{!! __('Latest News') !!}</h2>
+            <ul class="" data-aos="flip-left" data-aos-duration="500" data-aos-delay="50" class="aos-init aos-animate">
+                @php
+                // dd($category->id);
+                 $postsLatest = get_latest_posts_noncategory([
+                theme_option('default_category_news'), 
+                theme_option('default_category_newspaper')],3)
+        
+                @endphp
+                @if (!empty( $postsLatest))
+                @foreach ( $postsLatest as $post) 
+                <div class="post-new-item">
+                    <div class="post-thumbnail-wrap">
+                        <div class="post-thumbnail">
+                            <a href="{{$post->url}}"><img src="{{ Storage::disk('public')->exists($post->image) ? get_object_image($post->image, 'post-large') : RvMedia::getDefaultImage()}}" alt="{{$post->name}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="title font18">
+                        <a href="{{$post->url}}">{!!str::words($post->name,16)!!}</a>
+                        <p class="time">{{date_format($post->created_at,"d/m/Y")}}</p>
+                    </div>
+                </div>
+                @endforeach
+                @endif
+                
+            </ul>
+            
+        </div>
+    </div>
 </div>
-
-<script>
-   
-//    if (screen.width > 1080) {
-//     let scroll = "{{$check}}";
-//     if (scroll) {
-//         let position = $('.relationship-sibar').position();
-//         $('html, body').animate({
-//             scrollTop: position.top - 100
-//         }, 1000);
-
-//     }
-//     }
-   
-</script>
